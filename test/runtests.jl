87d448dc6b4d11034e40eb8c75ca07c67f761a7b
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2023-04-05 00:07:07 -0300

    :rotating_light: Improve test organization

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,40 +6,42 @@ using SatelliteToolboxBase
 using SatelliteToolboxTransformations
 using StaticArrays
 
-@testset "IAU-76 / FK5 theory" verbose = true begin
-    include("./fk5/nutation.jl")
-    include("./fk5/precession.jl")
-    include("./fk5/fk5.jl")
-end
+@testset "Reference frame transformations" verbose = true begin
+    @testset "IAU-76 / FK5 theory" verbose = true begin
+        include("./reference_frames/fk5/nutation.jl")
+        include("./reference_frames/fk5/precession.jl")
+        include("./reference_frames/fk5/fk5.jl")
+    end
 
-@testset "IAU 2006 / 2010A theory (CIO-based)" verbose = true begin
-    include("./iau2006/cio.jl")
-    include("./iau2006/precession.jl")
-    include("./iau2006/iau2006_cio.jl")
-end
+    @testset "IAU 2006 / 2010A theory (CIO-based)" verbose = true begin
+        include("./reference_frames/iau2006/cio.jl")
+        include("./reference_frames/iau2006/precession.jl")
+        include("./reference_frames/iau2006/iau2006_cio.jl")
+    end
 
-@testset "IAU 2006 / 2010A theory (Equinox-based)" verbose = true begin
-    include("./iau2006/nutation_eo.jl")
-    include("./iau2006/iau2006_equinox.jl")
-end
+    @testset "IAU 2006 / 2010A theory (Equinox-based)" verbose = true begin
+        include("./reference_frames/iau2006/nutation_eo.jl")
+        include("./reference_frames/iau2006/iau2006_equinox.jl")
+    end
 
-@testset "True Equator, Mean Equinox (TEME)" verbose = true begin
-    include("./teme/teme.jl")
-end
+    @testset "True Equator, Mean Equinox (TEME)" verbose = true begin
+        include("./reference_frames/teme/teme.jl")
+    end
 
-@testset "Conversions ECEF <=> ECI" verbose = true begin
-    include("./ecef_to_ecef.jl")
-    include("./ecef_to_eci.jl")
-    include("./eci_to_ecef.jl")
-    include("./eci_to_eci.jl")
-end
+    @testset "Conversions ECEF <=> ECI" verbose = true begin
+        include("./reference_frames/ecef_to_ecef.jl")
+        include("./reference_frames/ecef_to_eci.jl")
+        include("./reference_frames/eci_to_ecef.jl")
+        include("./reference_frames/eci_to_eci.jl")
+    end
 
-@testset "Conversions ECEF <=> Local frames" verbose = true begin
-    include("./local_frame.jl")
-end
+    @testset "Conversions ECEF <=> Local frames" verbose = true begin
+        include("./reference_frames/local_frame.jl")
+    end
 
-@testset "Conversions Geodetic <=> Geocentric" verbose = true begin
-    include("./geodetic_geocentric.jl")
+    @testset "Conversions Geodetic <=> Geocentric" verbose = true begin
+        include("./reference_frames/geodetic_geocentric.jl")
+    end
 end
 
 @testset "Orbit transformations" verbose = true begin
