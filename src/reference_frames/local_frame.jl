dbb4f7ec97d72f199cddad3e46f8dd04e212be42
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-05-24 16:54:32 -0300

    :books: Improve documentation

--- src/reference_frames/local_frame.jl
+++ src/reference_frames/local_frame.jl
@@ -69,7 +69,7 @@ function ecef_to_ned(
 end
 
 """
-    ned_to_ecef(r_ned::AbstractVector, lat::Number, lon::Number, h::Number; translate::Bool = false)
+    ned_to_ecef(r_ned::AbstractVector{T1}, lat::T2, lon::T3, h::T4; translate::Bool = false) -> SVector{3, T}
 
 Convert a vector `r_ned` represented in the local reference frame NED (North, East, Down) at
 the geodetic position `lat` [rad], `lon` [rad], and `h` [m] to the Earth-Centered,
@@ -78,6 +78,9 @@ Earth-Fixed (ECEF) frame.
 If `translate` is `false`, then this function computes only the rotation between NED and
 ECEF. Otherwise, it will also translate the vector considering the distance between the
 Earth's center and NED origin.
+
+The element type `T` of the returned vector is obtained by promoting `T1`, `T2`, `T3`, and
+`T4` to a float.
 
 # Remarks
 
